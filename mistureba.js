67a75f14abfe8d9ac26ede7a2026eb9b041c83f7
Marcos Cardoso <marcos.cardoso.55@gmail.com>
2014-10-21 10:33:24 -0200

    Atualização em click

--- mistureba.js
+++ mistureba.js
@@ -1557,7 +1557,7 @@ p.nominalBounds = new cjs.Rectangle(-55.6,-32.3,71.8,49.8);
 			context.gotoAndPlay("instrucoes");
 			
 			if (context.enableSounds)
-				playSound("click");
+				createjs.Sound.play("click"); //playSound("click");
 		}
 		
 		// added for phonegap
